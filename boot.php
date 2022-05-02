7cd7dc71fac4dfec6df0f9a7d60f703ea74b892b
Alexander Walther <aw@alexplus.de>
2022-05-02 11:13:05 +0200

    config-param module / template writedown

--- boot.php
+++ boot.php
@@ -4,7 +4,7 @@ if (\rex_addon::get('yform') && \rex_addon::get('yform')->isAvailable()) {
     rex_yform::addTemplatePath(rex_path::addon('plus_bs5', 'yformtemplates'));
 }
 
-if (rex::isBackend() && rex::isDebugMode()) {
-    // bs5::writeModule();
-    // bs5::writeTemplate();
+if (rex::isBackend() && rex::isDebugMode() && rex_config::get('plus_bs5', 'dev')) {
+    bs5::writeModule();
+    bs5::writeTemplate();
 }
