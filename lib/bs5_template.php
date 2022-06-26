7fd843e4989d11f32b3e5d21582de426bbccf847
Alexander Walther <aw@alexplus.de>
2022-06-26 16:31:17 +0200

    getAssetUrl

--- lib/bs5_template.php
+++ lib/bs5_template.php
@@ -2,4 +2,12 @@
 
 class bs5_template
 {
+    public static function getAssetUrl($file, $show_timestamp = true)
+    {
+        if ($show_timestamp) {
+            $timestamp = '?timestamp='.filectime(rex_path::assets($file));
+        }
+
+        return rex_url::assets($file) . $timestamp;
+    }
 }
