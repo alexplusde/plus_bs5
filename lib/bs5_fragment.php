3de530a116739751a9439bcb4f2f135924eb8192
Alexander Walther <aw@alexplus.de>
2023-04-25 20:03:02 +0200

    Update bs5_fragment.php

--- lib/bs5_fragment.php
+++ lib/bs5_fragment.php
@@ -2,16 +2,24 @@
 
 class bs5_fragment extends rex_fragment
 {
-    public function parse($fragment_frontend, $fragment_backend = null)
+    public function parse($fragment_frontend, $fragment_backend = "")
     {
-        if ($fragment_backend && rex::isBackend()) {
-            return parent::parse($fragment_backend);
+        if ($fragment_backend !== "" && rex::isBackend()) {
+            return parent::parse(self::addSuffix($fragment_backend));
         }
-        return parent::parse($fragment_frontend);
+        return parent::parse(self::addSuffix($fragment_frontend));
     }
-
     public function show($fragment_frontend, $fragment_backend = null)
     {
         echo self::parse($fragment_frontend, $fragment_backend);
+    }
+    
+    public static function addSuffix($filename = "", $suffix = ".php")
+    {
+
+        if(substr($filename, -strlen($suffix)) !== $suffix) {
+            return $filename.$suffix;
+        }
+        return $filename;
     }
 }
