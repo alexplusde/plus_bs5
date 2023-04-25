231574475665525b72496fa8ac413aa0336d5110
Alexander Walther <aw@alexplus.de>
2023-04-25 10:30:05 +0200

    fix forntend backend

--- lib/bs5_fragment.php
+++ lib/bs5_fragment.php
@@ -5,10 +5,11 @@ class bs5_fragment extends rex_fragment
     public function parse($fragment_frontend, $fragment_backend = null)
     {
         if ($fragment_backend && rex::isBackend()) {
-            return parent::parse($fragment_parent);
+            return parent::parse($fragment_backend);
         }
         return parent::parse($fragment_frontend);
     }
+
     public function show($fragment_frontend, $fragment_backend = null)
     {
         echo self::parse($fragment_frontend, $fragment_backend);
