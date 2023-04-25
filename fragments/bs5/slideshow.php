93d6cc4b7c83e43db54dc54a503dca13a3b8a1b5
Alexander Walther <aw@alexplus.de>
2023-04-25 20:04:43 +0200

    Update slideshow.php

--- fragments/bs5/slideshow.php
+++ fragments/bs5/slideshow.php
@@ -5,8 +5,7 @@ $images = explode(",", $this->getVar('images'));
 ?>
 
 <section class="bs5_gallery">
-	<div id="carousel-<?= $this->getVar('slice_id') ?>"
-		class="carousel slide" data-bs-ride="carousel">
+	<div id="carousel-REX_SLICE_ID" class="carousel slide" data-bs-ride="carousel">
 		<div class="carousel-indicators">
 
 			<?php
@@ -17,8 +16,7 @@ $active = "active";
 foreach ($images as $image) {
     if ($media = rex_media::get($image)) {
         ?>
-			<button type="button"
-				data-bs-target="#carousel-<?= $this->getVar('slice_id') ?>"
+			<button type="button" data-bs-target="#carousel-REX_SLICE_ID"
 				data-bs-slide-to="<?= $index++ ?>"
 				class="<?= $active ?>" aria-current="true"
 				aria-label="<?= $image ?>"></button>
@@ -40,12 +38,12 @@ foreach ($images as $image) {
 $index = 0;
 $active = "active";
 foreach ($images as $image) {
-    if ($media = rex_media::get($image)) {
+    if ($media = rex_media_plus::get($image)) {
         ?>
 			<div
 				class="carousel-item <?= $active; ?> ratio ratio-16x9">
 				<?php # media_plus::get($image)->getImg('addon_bs_fullhd');?>
-				<?= media_manager_type_group::getPicture('default', $image); ?>
+				<?= media_manager_type_group::getPicture('default', $media); ?>
 				<!--                    <img src="/media/addon_bs_fullhd/<?= $image ?>"
 				class="d-block w-100"
 				alt="<?= $media->getValue('med_title'); ?>">
@@ -72,11 +70,11 @@ foreach ($images as $image) {
 		<button class="carousel-control-prev" type="button" data-bs-target="#carousel-REX_SLICE_ID"
 			data-bs-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">{{bs5.slideshow.prev}}</span>
+			<span class="visually-hidden">Previous</span>
 		</button>
 		<button class="carousel-control-next" type="button" data-bs-target="#carousel-REX_SLICE_ID"
 			data-bs-slide="next">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="visually-hidden">{{bs5.slideshow.next}}</span>
+			<span class="visually-hidden">Next</span>
 		</button>
 </section>
