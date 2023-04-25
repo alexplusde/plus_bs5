21eeaf0367914dfcb26b37458eb13c472e34faf1
Alexander Walther <aw@alexplus.de>
2023-04-25 20:03:14 +0200

    Update text.php

--- fragments/bs5/text.php
+++ fragments/bs5/text.php
@@ -1,6 +1,8 @@
 <section class="modul modul-text bs5-text">
 	<div
 		class="container <?= rex_config::get("plus_bs5", "container_class") ?>">
+		<?php if ($this->getVar('content') !== "") { ?>
 		<?= $this->getVar('content') ?>
+		<?php } ?>
 	</div>
 </section>
