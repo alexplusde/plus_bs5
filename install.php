51e37a8c6d9b60c5aa8e0d1662593be57f34b42c
Alexander Walther <mail@alexplus.de>
2024-04-07 23:43:50 +0200

    Update install.php

--- install.php
+++ install.php
@@ -3,4 +3,6 @@
 bs5::updateModule();
 bs5::updateTemplate();
 
-rex_metainfo_add_field('translate:cat_nav', 'cat_nav', '100', '', 5, '||', 'desktop_hidden:translate:plus_bs5_nav_desktop_hidden');
+if(rex_addon::get('metainfo')->isAvailable()) {
+    rex_metainfo_add_field('translate:cat_nav', 'cat_nav', '100', '', 5, '||', 'desktop_hidden:translate:plus_bs5_nav_desktop_hidden');
+}
