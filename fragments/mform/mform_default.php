915215f258177f00eeb7bfe64e16701629b59a93
Alexander Walther <mail@alexplus.de>
2024-12-11 01:54:26 +0100

    MFrom-Default überschreiben (100% Label-Breite), Anpassung an MForm 8

---
+++ fragments/mform/mform_default.php
@@ -0,0 +1,23 @@
+<?php
+/** @var rex_fragment $this */
+
+switch ($this->getVar('type')) {
+    default:
+    case 'default':
+        $this->setVar('labelColClass', 'col-md-12');
+        $this->setVar('formItemColClass', 'col-md-12');
+        break;
+    case 'default_full':
+    case 'default_custom_full':
+        $this->setVar('labelColClass', 'col-sm-12');
+        $this->setVar('formItemColClass', 'col-sm-12');
+        break;
+}
+
+$labelWrapperOpen = '<div class="' . $this->getVar('labelColClass') . '">';
+$labelWrapperClose = '</div>';
+$inputWrapperOpen = '<div class="' . $this->getVar('formItemColClass') . '">';
+$inputWrapperClose = ($this->getVar('notCloseInputWrapper')) ? '' : '</div>';
+$formGroupClose = ($this->getVar('notClosedFormGroup')) ? '' : '</div>';
+
+echo '<div class="form-group ' . $this->getVar('class') . '">' . $labelWrapperOpen . $this->getVar('label') . $this->getVar('infoTooltip') . $this->getVar('infoCollapseButton') . $labelWrapperClose . $inputWrapperOpen . $this->getVar('element') . $inputWrapperClose . $formGroupClose . $this->getVar('infoCollapse');
