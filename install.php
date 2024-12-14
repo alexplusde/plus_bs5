0db777d444127cd863d9ec27f536ab584d56ff19
Alexander Walther <mail@alexplus.de>
2024-12-14 17:53:13 +0100

    search_it Standard-Konfiguration

--- install.php
+++ install.php
@@ -12,3 +12,5 @@ if (rex_addon::get('metainfo') && rex_addon::get('metainfo')->isAvailable()) {
     rex_metainfo_add_field('translate:plus_bs5.metainfo.cat_nav', 'cat_nav', '100', '', 5, '||', 'desktop_hidden:translate:plus_bs5_nav_desktop_hidden');
     rex_metainfo_add_field('translate:plus_bs5.metainfo.clang_locale', 'clang_locale', '100', '', 1, '|"de_DE.utf8","de_DE","de"|', '');
 }
+
+rex_config::set("search_it", "plaintext_selectors", "head,script,link,header,footer,styles,[data-search_it=\"noindex\"]");
