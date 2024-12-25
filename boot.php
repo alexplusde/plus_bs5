a2b3cf1adfefa3f2a738373ee5fad03ea69878b8
Alexander Walther <mail@alexplus.de>
2024-12-25 18:11:55 +0100

    set clang in boot.php

--- boot.php
+++ boot.php
@@ -29,15 +29,21 @@ if (rex::isFrontend()) {
     rex_extension::register('OUTPUT_FILTER', static function (rex_extension_point $ep) {
         $subject = $ep->getSubject();
         $search = '<table>';
-        $replace = '<table class="table">';
+        $replace = '<table class="table table-responsive">';
         $subject = str_replace($search, $replace, $subject);
         $ep->setSubject($subject);
     });
 }
 
-if(rex::isBackend() && count(\rex_category::getRootCategories()) === 1 && \rex_be_controller::getCurrentPagePart(1) === 'structure' && \rex_be_controller::getCurrentPagePart(2) === '') {
-    if(rex_get('category_id', 'int', 0) === 0 && rex_get('clang', 'int', 0) === 0) {
+if (rex::isBackend() && count(\rex_category::getRootCategories()) === 1 && \rex_be_controller::getCurrentPagePart(1) === 'structure' && \rex_be_controller::getCurrentPagePart(2) === '') {
+    if (rex_get('category_id', 'int', 0) === 0 && rex_get('clang', 'int', 0) === 0) {
         rex_response::sendRedirect(\rex_url::backendPage('structure', ['category_id' => \rex_category::getRootCategories()[0]->getId()], false));
     }
 
 }
+
+$clang = \rex_clang::getCurrent();
+setlocale(LC_ALL, $clang->getValue('locale'), $clang->getCode());
+
+$addon = rex_addon::get('plus_bs5');
+$addon->setProperty('css', []);
