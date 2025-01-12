332a6e84cb22e880aa5ce5779ba8626de0cf5264
Alexander Walther <mail@alexplus.de>
2025-01-12 08:40:29 +0100

    Update Template.php

--- lib/Template.php
+++ lib/Template.php
@@ -9,7 +9,7 @@ class Template
     public static function addCss(array $css): void
     {
         $addon = rex_addon::get('plus_bs5');
-        $styles = $addon->getProperty('css');
+        $styles = $addon->getProperty('css') ?? [];
         $styles[$css['href']] = $css;
         $addon->setProperty('css', $styles);
     }
@@ -17,7 +17,7 @@ class Template
     public static function showCss(): void
     {
         $addon = rex_addon::get('plus_bs5');
-        $css = $addon->getProperty('css');
+        $css = $addon->getProperty('css') ?? [];
         foreach ($css as $c) {
             echo '<link '.\rex_string::buildAttributes($c ?? []).' rel="stylesheet" type="text/css">';
         }
@@ -27,16 +27,18 @@ class Template
     public static function addJs(array $js): void
     {
         $addon = rex_addon::get('plus_bs5');
-        $javascript = $addon->getProperty('js');
+        $javascript = $addon->getProperty('js') ?? [];
         $javascript[$js['src']] = $js;
         $addon->setProperty('js', $javascript);
     }
 
     public static function showJs(): void
     {
+
         $addon = rex_addon::get('plus_bs5');
-        $js = $addon->getProperty('js');
-        foreach ($js as $j) {
+        $js = $addon->getProperty('js') ?? [];
+     
+           foreach ($js as $j) {
             echo '<script '.\rex_string::buildAttributes($j ?? []).'></script>';
         }
     }
